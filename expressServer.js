af2bd262ba0ce7810c1e3fba83bc3fbcdaaaf4d6
Prateek Diwedi <diwedi1112@gmail.com>
2019-12-10 00:07:31 +0000

    added ejs dependencies

--- expressServer.js
+++ expressServer.js
@@ -2,14 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+// set the view engine to ejs  ****
+app.set('view engine', 'ejs');
+
+
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9ssh3u': 'http://www.google.com',
   '7digw6': 'http://www.shanti-enterprises.com'
 };
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
+app.get('/', (req, res) => {      // root route
+  res.send("Hello World");
 });
 
 app.get('/urls.json', (req, res) => {
